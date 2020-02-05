1a05715b61d2587aa815f303535e590aba5d52d7
Elie Chaaya <echaaya@icc.economy.gov.lb>
2020-02-05 12:01:58 +0200

    Updates

---
+++ precache-manifest.733210061696b04045a3a2741eaee576.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13e707181e58ce33cc2da0cceff07976",
+    "url": "/animals/index.html"
+  },
+  {
+    "revision": "9b71f2d6e4a6e8e40e52",
+    "url": "/animals/static/css/main.a50e0443.chunk.css"
+  },
+  {
+    "revision": "209da3af51d5ee449708",
+    "url": "/animals/static/js/2.02c6af30.chunk.js"
+  },
+  {
+    "revision": "eb16858e21cecd78a7f3de272899fffc",
+    "url": "/animals/static/js/2.02c6af30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b71f2d6e4a6e8e40e52",
+    "url": "/animals/static/js/main.2f664c1a.chunk.js"
+  },
+  {
+    "revision": "9c7aedeb5dc3339a97bb",
+    "url": "/animals/static/js/runtime-main.d74122f1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/animals/precache-manifest.fe38e27cb66cf3479b0be524c75ca880.js"
+  "/animals/precache-manifest.733210061696b04045a3a2741eaee576.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/animals/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/animals/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
