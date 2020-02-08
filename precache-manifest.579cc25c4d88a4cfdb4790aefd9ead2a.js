a791eefbfbf75d9fe169b92a0304cd49b1d30fc4
Elie Chaaya <echaaya@icc.economy.gov.lb>
2020-02-08 22:41:36 +0200

    Updates

--- precache-manifest.579cc25c4d88a4cfdb4790aefd9ead2a.js
+++ precache-manifest.579cc25c4d88a4cfdb4790aefd9ead2a.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "772a75eba704e6e571f5f43df4c37eba",
+    "revision": "68402070838bc6dcdda148e77918c377",
     "url": "/animals/index.html"
   },
   {
-    "revision": "3e992724e23deef6545a",
+    "revision": "9ab4f4fa2882fab5b867",
     "url": "/animals/static/css/main.33b63821.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/animals/static/js/2.02c6af30.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3e992724e23deef6545a",
-    "url": "/animals/static/js/main.619af520.chunk.js"
+    "revision": "9ab4f4fa2882fab5b867",
+    "url": "/animals/static/js/main.c4d038a3.chunk.js"
   },
   {
     "revision": "9c7aedeb5dc3339a97bb",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/animals/precache-manifest.1f76208f24733c6b53d7e4a51d76a9a3.js"
+  "/animals/precache-manifest.579cc25c4d88a4cfdb4790aefd9ead2a.js"
 );
 
 self.addEventListener('message', (event) => {
